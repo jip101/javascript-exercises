ed8b24aef3c5b151916f248a93b6301d8bf7616f
Jeremy <jip101@gmail.com>
2023-01-21 02:15:14 -0500

    complete exercises 1-7

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(list, ...remove) {
+    for (let item = list.length - 1; item >= 0; item--) {
+        for (let value of remove){
+            if (list[item] === value) {
+                list.splice(item, 1);
+            }
+        }
+    }
+    return list;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  let c = (f - 32) * (5/9);
+  c = +c.toFixed(1);
+  return c;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  let f = (c * 9/5) + 32;
+  f = +f.toFixed(1);
+  return f;
 };
 
 // Do not edit below this line
